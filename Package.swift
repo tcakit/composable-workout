88ec8e61d6dfe8187f1f476c427dd9bd609e73ce
Joe Blau <im@joeblau.com>
2021-03-01 10:28:06 -0800

    Dependency version bump

--- Package.swift
+++ Package.swift
@@ -16,7 +16,7 @@ let package = Package(
             targets: ["ComposableWorkout"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/pointfreeco/swift-composable-architecture", from: "0.9.0")
+        .package(url: "https://github.com/pointfreeco/swift-composable-architecture", from: "0.15.0")
     ],
     targets: [
         .target(name: "ComposableWorkout", dependencies: [
